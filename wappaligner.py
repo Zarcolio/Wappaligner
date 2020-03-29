9e9c05b6beb609676bc358a2d2c5cf84fc3b21b2
Zarcolio <registered@quicknet.nl>
2020-03-29 01:30:45 +0100

    Add files via upload

---
+++ wappaligner.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import json
+import sys
+import urllib.parse
+from termcolor import colored
+
+sStandardInput = ""
+
+for x in sys.stdin:
+    sStandardInput = sStandardInput + x 
+
+#print (sStandardInput)
+try:
+    json_data = json.loads(sStandardInput)
+except:
+    pass
+
+   
+for sUrl in json_data["urls"]:
+    print (colored("URL = " + sUrl + " (HTTP status "+ str(json_data['urls'][sUrl]['status']) + ")","green"))
+#    print (colored("URL = " + sUrl["status"],"green"))
+
+for app in json_data["applications"]:
+    if app["version"]:
+        sVersion1 = " " + str(app["version"])
+        sVersion2 = " \"" + str(app["version"]) + "\""  
+    else:
+        sVersion1 = ""
+        sVersion2 = ""
+
+    sExploitdbUrl = "https://www.exploit-db.com/search?q=" + urllib.parse.quote(app["name"])
+    sGoogleUrl = "https://www.google.com/search?q=" + urllib.parse.quote("\"" + app["name"] + "\"" + sVersion2 + " cve |exploit |vulnerability |update |changelog |risk |advisory |cvss")
+    print (app["name"] + sVersion1 + " (" + app["confidence"] + "%)")
+    print (" - " + app["website"])
+    print (" - " + sExploitdbUrl)
+    print (" - " + sGoogleUrl)
+    
---
+++ wappalyzer-installer.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo apt install -y nodejs npm
+
+
+sudo npm i -g commander
+sudo npm i -g normalize-url
+sudo npm i -g url
+sudo npm i -g wappalyzer
+sudo npm i -g puppeteer
+sudo npm i -g wappalyzer-cli
+

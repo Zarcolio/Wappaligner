d980a59a8b9b32d378a9c6f2402a97ac10502c98
Zarcolio <registered@quicknet.nl>
2023-03-19 13:56:29 +0100

    optimized the code

--- wappaligner.py
+++ wappaligner.py
@@ -6,38 +6,38 @@ import sys
 import urllib.parse
 from termcolor import colored
 
-sStandardInput = ""
-
-for x in sys.stdin:
-    sStandardInput = sStandardInput + x 
-
-#print (sStandardInput)
 try:
-    json_data = json.loads(sStandardInput)
-except:
-    pass
+    json_data = json.load(sys.stdin)
+except json.JSONDecodeError:
+    print("Error: Invalid JSON input.")
+    sys.exit(1)
+except Exception as e:
+    print("Error:", e)
+    sys.exit(1)
 
-   
-for sUrl in json_data["urls"]:
-    print (colored("URL = " + sUrl + " (HTTP status "+ str(json_data['urls'][sUrl]['status']) + ")","green"))
-#    print (colored("URL = " + sUrl["status"],"green"))
+if "urls" not in json_data or "applications" not in json_data:
+    print("Error: Invalid input format.")
+    sys.exit(1)
+
+for sUrl, data in json_data["urls"].items():
+    status = data.get("status", "unknown")
+    print(colored(f"URL = {sUrl} (HTTP status {status})", "green"))
 
 for app in json_data["applications"]:
-    if app["version"]:
-        sVersion1 = " " + str(app["version"])
-        sVersion2 = " \"" + str(app["version"]) + "\""  
-    else:
-        sVersion1 = ""
-        sVersion2 = ""
-
-    sExploitdbUrl = "https://www.exploit-db.com/search?q=" + urllib.parse.quote(app["name"])
-    sinfosecmatterUrl = "https://www.google.com/search?q=site%3Ainfosecmatter.com+%22Public+Exploits%22+" + urllib.parse.quote("\"" + app["name"]+ "\"")
-    sTenableUrl = "https://www.google.com/search?q=site%3Atenable.com/plugins+" + urllib.parse.quote("\"" + app["name"]+ "\"")
-    sGoogleUrl = "https://www.google.com/search?q=" + urllib.parse.quote("\"" + app["name"] + "\"" + sVersion2 + " cve |exploit |vulnerability |update |changelog |risk |advisory |cvss")
-    print (app["name"] + sVersion1 + " (" + app["confidence"] + "%)")
-    print (" - " + app["website"])
-    print (" - " + sExploitdbUrl)
-    print (" - " + sTenableUrl)
-    print (" - " + sinfosecmatterUrl)
-    print (" - " + sGoogleUrl)
+    name = app["name"]
+    version = app.get("version", "")
+    confidence = app["confidence"]
+    sVersion1 = f" {version}" if version else ""
+    sVersion2 = f" \"{version}\"" if version else ""
     
+    sExploitdbUrl = f"https://www.exploit-db.com/search?q={urllib.parse.quote(name)}"
+    sinfosecmatterUrl = f"https://www.google.com/search?q=site%3Ainfosecmatter.com+%22Public+Exploits%22+{urllib.parse.quote(f'"{name}"')}"
+    sTenableUrl = f"https://www.google.com/search?q=site%3Atenable.com/plugins+{urllib.parse.quote(f'"{name}"')}"
+    sGoogleUrl = f"https://www.google.com/search?q={urllib.parse.quote(f'"{name}"{sVersion2} cve |exploit |vulnerability |update |changelog |risk |advisory |cvss')}"
+    
+    print(f"{name}{sVersion1} ({confidence}%)")
+    print(f" - {app['website']}")
+    print(f" - {sExploitdbUrl}")
+    print(f" - {sTenableUrl}")
+    print(f" - {sinfosecmatterUrl}")
+    print(f" - {sGoogleUrl}")
